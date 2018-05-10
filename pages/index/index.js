2b0a4f7797866f4e146703480fe6fb371a2ebb14
timedivision <qiaoziqiangand@163.com>
2018-05-10 18:08:24 +0800

    速度问题需要调整

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,7 +14,7 @@ Page({
             },
             {
                 'siteNum': 8,
-                'name': '4块钱'
+                'name': '8块钱'
             },
             {
                 'siteNum': 0,
@@ -22,19 +22,19 @@ Page({
             },
             {
                 'siteNum': 4,
-                'name': '5块钱'
+                'name': '4块钱'
             },
             {
                 'siteNum': 7,
-                'name': '6块钱'
-            },
-            {
-                'siteNum': 6,
                 'name': '7块钱'
             },
             {
+                'siteNum': 6,
+                'name': '6块钱'
+            },
+            {
                 'siteNum': 5,
-                'name': '8块钱'
+                'name': '5块钱'
             }
         ],
         selectIndex: 1      //中奖信息及跑马灯ui  默认第一个,同siteNum对比
@@ -42,43 +42,47 @@ Page({
 
     start: function () {
         var that = this;
-        var awards = this.data.awards; //奖品
+        var awards = this.data.awards; //奖品列表
         var selectIndex = this.data.selectIndex; //中奖信息
         var len = this.data.awards.length;  //奖品长度
-        var speed = 0;
+        var speed = 500;
         var stopNum = (Math.random() * len) >>> 0; //获取奖品
-            // stopNum = 5; //获取奖品
+        stopNum = 5;
         if (!stopNum) {
             stopNum = (Math.random() * len) >>> 0; //如果取到0就重新来一次
         }
-        stopNum = 3 * (len - 1) + stopNum; //转三圈加上中奖步长
-        console.log(stopNum)
-        console.log(stopNum - 3 * (len - 1));
-        var timer = setInterval(function () {
+        for (var i in awards) {
+            if (awards[i].siteNum == stopNum) { //根据索引去对比奖品的siteNum,相同就取得奖品名
+                var name = awards[i].name;
+            }
+        }
+        wx.showToast({
+            title:name
+        })
+        stopNum = 3 * (len - 1) + stopNum - selectIndex; //转三圈加上中奖步长,减去上一次的中奖步长
+        var timer = null;
+        function run(){
             selectIndex += 1; //跑马灯ui
-            if (selectIndex > len) {
+            if (selectIndex == len) {
                 selectIndex = 1;
             }
             that.setData({
                 selectIndex: selectIndex //赋值操作更改class
             })
             stopNum -= 1;
-            if (stopNum == 0) {
+            if (stopNum <= 0) {
                 clearInterval(timer);
-                for (var i in awards) {
-                    if (awards[i].siteNum == selectIndex) { //根据索引去对比奖品的siteNum,相同就取得奖品名
-                        var name = awards[i].name;
-                    }
-                }
                 wx.showModal({
                     title: '提示',
                     content: '恭喜您获得' + name,
                     showCancel: false
                 })
             }
-        }, (100))
-        // var timer1 = setInterval(function () {
-        // },100)
+        }
+        console.log( stopNum * 100) //总共花的时长
+        timer = setInterval(function () {
+            run();
+        }, 100)
     },
     onLoad: function (options) {
         //Do some initialize when page load.
